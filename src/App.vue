32008bb4881cd654e0b3cc407a26f52003bbd98a
chakrit072 <watachikrit2@gmail.com>
2020-04-01 10:22:31 +0100

    homework.krit

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,36 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <!-- <router-view/> -->
+    <h1 id="wep">{{namewep}}</h1>
+    <h2>Your name {{massages}}</h2>
+    <input v-model="massages" type="text">
+    <button v-on:click="isHidden=!isHidden">click</button>
+    <button v-on:click="alert()">alert</button>
+    <h2 v-if="!isHidden" id="alert">Hello {{massages}}!</h2>
   </div>
-</template>
 
+
+</template>
+<script>
+export default {
+  data(){
+    return{
+      namewep:"!WELCOME!",
+      massages:"",
+      isHidden: true
+
+    }
+  },
+  methods:{
+    alert: function(){
+      return alert('Hello '+this.massages)
+    }
+  }
+  
+}
+
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -28,5 +54,11 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+#wep{
+  color: blue;
+}
+#alert{
+  color: red;
 }
 </style>
